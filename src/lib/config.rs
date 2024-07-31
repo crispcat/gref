77a184a08951009e9a21887e43159d303046124b
enkie <enkiespearo@proton.me>
2024-07-31 17:59:11 +0300

    @

--- src/lib/config.rs
+++ src/lib/config.rs
@@ -67,50 +67,58 @@ impl Config {
                 "-e" => {
                     match parse_param_value(&mut args_iterator) {
                         Ok(val) => config.groups_to_extract.push(val),
-                        Err(er) => errors.push(er)
+                        Err(err) => errors.push(err)
                     }
                 },
                 "-f" => {
                     match parse_param_value(&mut args_iterator) {
                         Ok(val) => output_format = Some(val),
-                        Err(er) => errors.push(er)
+                        Err(err) => errors.push(err)
                     }
                 },
                 "-p" => {
                     match parse_param_value(&mut args_iterator) {
                         Ok(val) => config.sources.push(Arc::new(PlainText(val))),
-                        Err(er) => errors.push(er)
+                        Err(err) => errors.push(err)
                     }
                 },
                 "-t" => {
                     match parse_param_value(&mut args_iterator) {
                         Ok(val) => threads_count = Some(val),
-                        Err(er) => errors.push(er)
+                        Err(err) => errors.push(err)
                     }
                 },
                 "-v" => {
-                    config.verbose = args_iterator.next().map(|_| true).unwrap()
+                    config.verbose = true;
+                    args_iterator.next();
                 },
                 "-i" => {
-                    config.case_insensitive = args_iterator.next().map(|_| true).unwrap()
+                    config.case_insensitive = true;
+                    args_iterator.next();
                 },
                 "-m" => {
-                    config.multiline = args_iterator.next().map(|_| true).unwrap()
+                    config.multiline = true;
+                    args_iterator.next();
                 },
                 "-s" => {
-                    config.dot_matches_new_line = args_iterator.next().map(|_| true).unwrap()
+                    config.dot_matches_new_line = true;
+                    args_iterator.next();
                 },
                 "-U" => {
-                    config.swap_greed = args_iterator.next().map(|_| true).unwrap()
+                    config.swap_greed = true;
+                    args_iterator.next();
                 },
                 "-x" => {
-                    config.ignore_whitespace_and_comments = args_iterator.next().map(|_| true).unwrap()
+                    config.ignore_whitespace_and_comments = true;
+                    args_iterator.next();
                 },
                 "-u" => {
-                    config.unicode = args_iterator.next().map(|_| true).unwrap()
+                    config.unicode = true;
+                    args_iterator.next();
                 },
                 "-d" => {
-                    config.debug_mode = args_iterator.next().map(|_| true).unwrap()
+                    config.debug_mode = true;
+                    args_iterator.next();
                 },
                 _ => {
                     let arg = args_iterator.next().unwrap();
@@ -121,10 +129,10 @@ impl Config {
                         continue;
                     }
                     match search_expr {
-                        None    => search_expr = Some(arg),
+                        None => search_expr = Some(arg),
                         Some(_) => match stat_fs_source(arg) {
                             Ok(source) => config.sources.push(Arc::new(source)),
-                            Err(error) => errors.push(error)
+                            Err(err) => errors.push(err)
                         }
                     }
                 }
@@ -177,7 +185,7 @@ fn stat_fs_source(arg: String) -> Result<TextSource, ConfigError> {
     let exists = Path::try_exists(path).map_err(|e| { format!("Cannot stat fs.\n{e}") })?;
     match exists {
         true => match Path::is_dir(path) {
-            true  => Ok(DirPath(arg)),
+            true => Ok(DirPath(arg)),
             false => Ok(FilePath(arg))
         }
         false => Err(format!("File or directory {arg} is not exist!"))
